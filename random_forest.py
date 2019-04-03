65251cc62d5c72e32c4ebd89a7d58d1b013c531e
Danilo Santos <danilo.santosrs@gmail.com>
2019-04-03 12:25:51 -0300

    work-03-04-2019

--- random_forest.py
+++ random_forest.py
@@ -29,6 +29,39 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 # [4]
 from sklearn.ensemble import RandomForestClassifier
+# [5]
+from sklearn.metrics import accuracy_score
+# [6]
+from matplotlib.colors import ListedColormap
+
+
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+	# setup marker generator and color map
+	markers = ('s', 'x', 'o', '^', 'v')
+	colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+	cmap = ListedColormap(colors[:len(np.unique(y))])
+	# plot the decision surface
+	x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+	x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+	xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+	np.arange(x2_min, x2_max, resolution))
+	Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+	Z = Z.reshape(xx1.shape)
+	plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+	plt.xlim(xx1.min(), xx1.max())
+	plt.ylim(xx2.min(), xx2.max())
+	# plot all samples
+	X_test, y_test = X[test_idx, :], y[test_idx]
+	for idx, cl in enumerate(np.unique(y)):
+		plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+		alpha=0.8, c=cmap(idx),
+		marker=markers[idx], label=cl)
+	# highlight test samples
+	if test_idx:
+		X_test, y_test = X[test_idx, :], y[test_idx]
+		plt.scatter(X_test[:, 0], X_test[:, 1], c='', alpha=1.0, linewidth=1, marker='o', s=55, label='test set')
+
 
 """
 Atribuiremos o comprimento da pétala e a largura da pétala das 150 amostras de flores à matriz 
@@ -86,8 +119,29 @@ One-vs.-Rest (OvR), que nos permite alimentar as três classes de flores ao perc
 #[4]
 forest = RandomForestClassifier(criterion='entropy', n_estimators=10, random_state=1, n_jobs=2)
 forest.fit(X_train, y_train)
+
+"""
+O Scikit-learn também implementa uma grande variedade de diferentes métricas de desempenho que estão 
+disponíveis através do módulo de métricas. Por exemplo, podemos calcular a precisão da classificação 
+do perceptron no conjunto de testes da seguinte forma:
+
+Aqui, y_test são os verdadeiros rótulos de classe e y_pred são os rótulos de classe que previmos 
+anteriormente.
+"""
+#[5]
+##y_pred = classifier.predict(X_test_std)
+##print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+
+"""
+Com a pequena modificação que fizemos na função plot_decision_regions (destacada no código anterior), 
+podemos agora especificar os índices das amostras que queremos marcar nos gráficos resultantes. 
+"""
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
 plot_decision_regions(X_combined, y_combined, classifier=forest, test_idx=range(105,150))
 plt.xlabel('petal length')
 plt.ylabel('petal width')
 plt.legend(loc='upper left')
 plt.show()
+
+
